cc14e654180053abbeaade1b8dcba00080cfa9cd
mfrias <mfrias@financialsolutions.mx>
2021-03-20 11:23:16 -0600

    Creacion de login, sidebar y Pagina inicio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
+import React from 'react'
+import Index from './componentes/Index/Index.jsx'
+import SideBar from './componentes/Hamburguesa/sidebar.jsx'
+import Login from './componentes/Login/Login.jsx'
+import './App.css'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>
-        Pablo
-      </h1>
+    <div className="App">
+      <SideBar pageWrapId={"container"} outerContainerId={"App"} />
+    <div className="container">
+      <Router>
+      <Switch>
+          <Route path="/Venta">
+            
+          </Route>
+          <Route path="/Login">
+            <Login />
+          </Route>
+          <Route path="/Productos">
+            
+          </Route>
+          <Route path="/Ingredientes">
+            
+          </Route>
+          <Route path="/Empleados">
+            
+          </Route>
+          <Route exact path="/">
+            <Index />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
     </div>
   );
 }
