136373fbf755a5133378406ae95585ec37b1468f
ximenaTo <56921602+ximenaTo@users.noreply.github.com>
2021-03-19 18:26:33 -0600

    Bootstrap
    Instalacion de bootstrap y en app.js esta el import del bootstrap y un ejemplo para ver si jalo xd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div>
+      <button className="btn btn-success">Pris</button>
       <h1>pris</h1>
     </div>
   );
